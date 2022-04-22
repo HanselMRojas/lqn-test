33b23bf7c89d0fffc0e4cbe4ebca663c6c5be64e
Hansel M. Rojas <co.hanselmorales@gmail.com>
2022-04-22 10:50:19 -0500

    feat (init): initialize project and integrate mui 5

---
+++ pages/tests/part-two.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const PartOne: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>LQN Test Part Two</title>
+        <meta name="description" content="LQN Test Part Two" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>LQN TEST PART 2</h1>
+    </div>
+  )
+}
+
+export default PartOne
